a92ae7b7d3f8dc42ded2c4006b9bd80da4142154
Rizwan <fbriz007@gmail.com>
2022-04-14 23:58:41 +0530

    Fix Navbar covering products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Grid}  from '@material-ui/core';
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id:1, name: "Tea", desc: "Assam Tea", price: "Rs. 99", image: "https://media.istockphoto.com/photos/black-tea-in-a-cup-picture-id475614605?k=20&m=475614605&s=612x612&w=0&h=QxyE7nnKJi2m9xoOedKiuLmblGT-87daZPkvHnPP9eY="},
@@ -9,9 +10,11 @@ const products = [
 ];
 
 const Products = () => {
-    
+
+    const classes = useStyles();
     return(
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justify='center' spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
