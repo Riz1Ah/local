87f67c010f57ec541fa097eaf133b2fae2ce50f6
Rizwan <fbriz007@gmail.com>
2022-04-15 17:36:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { CssBaseline } from '@material-ui/core';
 import { Products, Navbar, Cart, Checkout } from './components';
 import { commerce } from './lib/commerce';
 
 const App = () => {
 
+    const [mobileOpen, setMobileOpen] = React.useState(false);
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+
     
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -40,28 +44,48 @@ const App = () => {
     
         setCart(response.cart);
       };
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
     
+        setCart(newCart);
+      };
+    
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+          const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+    
+          setOrder(incomingOrder);
+    
+          refreshCart();
+        } catch (error) {
+          setErrorMessage(error.data.error.message);
+        }
+      };
     
        
     useEffect(() => {
         fetchProducts();
         fetchCart();
       }, []);
+      
+    const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
     
     return ( 
-        <Router>
-            <div>
-                <Navbar totalItems={cart.total_items} />
-                <Routes>
-                    <Route exact path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>} />
-                    
-                    <Route exact path='/cart' element={<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart}/> } />
-                    <Route path="/checkout" exact element={<Checkout cart={cart} />}/>
-                </Routes>
-                
-            </div>
-        </Router>
-         );
+      <Router>
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Routes>
+          <Route exact path='/' element={<Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty/>} />
+
+          <Route exact path='/cart' element={<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart}/> } />
+          <Route path="/checkout" exact element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />}/>
+
+        </Routes>
+      </div>
+    </Router>
+  );
+        
 }
  
 export default App;
